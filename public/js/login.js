71a7a40dfd5bee6caf6eafc0b5cd40e18f1688df
amit0617 <71893015+Amit0617@users.noreply.github.com>
2022-03-28 16:59:07 +0530

    code quality

--- public/js/login.js
+++ public/js/login.js
@@ -20,16 +20,24 @@ window.onload = async () => {
         await auth.loginWithSocial(SocialLoginType.google);
 
         if (auth.isLoggedIn()) {
-            //remove sign in button
+            //remove sign in button and sign up CTA
             const not_required = document.getElementsByTagName('a');
             not_required[0].parentNode.remove(not_required);
             document.getElementsByTagName('span')[1].parentNode.removeChild(document.getElementsByTagName('span')[1]);
+            
+            // get data from sign up
             const info = auth.getUserInfo();
-            console.log(info);
+            //console.log(info);
+            
+            // create image for user photo from gmail
             const userPic = document.createElement('img');
             userPic.src = info.userInfo.picture;
+            
+            //put into navbar
             const navBar = document.getElementById('navBar');
             navBar.appendChild(userPic);
+            
+            //align to right corner
             userPic.classList.add('userPic');
         }
     })
--- public/js/redirect.js
+++ public/js/redirect.js
@@ -1,3 +1,2 @@
-// window.onload = async () => {
+//redirect back to the main page closing the popup
     AuthProvider.handleRedirectPage(`http://localhost:3000/`);
-//   };
